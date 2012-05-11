3147294f377c8c6163196cf007d4a8eeca77d388
gh-gmgw <gmgwmt@gmail.com>
2012-05-11 19:09:04 +0900

    Add createExcel.pl

---
+++ sample/Excel/createExcel.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Win32::OLE;
+use Win32::OLE::Const 'Microsoft Excel';
+use Encode;
+
+# エラー発生時のWin32::OLE モジュールの動きを指定（Carp::croak）
+Win32::OLE->Option(Warn => 3);
+
+# Excelの有無確認
+eval {
+	Win32::OLE->GetActiveObject('Excel.Application');
+};
+if ($@) {
+	die "Excelが入っていません $@";
+}
+
+# Excelの起動
+my $excel = Win32::OLE->GetActiveObject('Excel.Application') || Win32::OLE->new('Excel.Application', 'Quit');
+if (!defined($excel)) {
+	die "Excelが起動できません";
+}
+
+# メッセージを抑制する（これをしないといろんなところでダイアログが出てしまう）
+$excel->{DisplayAlerts} = 'False';
+
+# workbookを作成する
+$excel->{SheetsInNewWorkbook} = 1;
+my $book = $excel->Workbooks->Add;
+
+# 作った段階で付与される余計なものを調整
+# xlRDIPrinterPath,xlRDIDocumentProperties,xlRDIRemovePersonalInformationをRemoveDocumentInformationで削除
+$book->RemoveDocumentInformation(xlRDIPrinterPath);
+$book->RemoveDocumentInformation(xlRDIDocumentProperties);
+$book->RemoveDocumentInformation(xlRDIRemovePersonalInformation);
+
+
+# sheetを作成する
+my $sheet = $book->Worksheets(1);
+my $ExcelFileName = 'c:\test.xls';
+
+# このスクリプトがutf8で書かれているので、Excelに出す場合はShiftJIS(CP932)に変換する必要有
+my $value = "て～すと①";
+Encode::from_to($value, "utf8", "CP932");
+
+# セルに出力
+$sheet->Cells(1,1)->{Value} = $value;
+
+
+# 保存してclose
+$book->SaveAs({Filename => "$ExcelFileName", Fileformat => 56});
+$book->Close();
+
+# Excelの終了
+$excel->Quit();
+
+__END__
